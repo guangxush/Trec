1813626f19d5aef5b1907e31246b596d3f7b921e
guangxush <guangxush@163.com>
2018-04-03 16:24:33 +0800

    mlpv2.0

--- cab_mlp.py
+++ cab_mlp.py
@@ -2,13 +2,13 @@ from __future__ import print_function
 from keras import losses
 from keras import Sequential
 from keras.layers import Dense,Dropout
-from keas.metrics import ModelCheckpoint, EarlyStopping, CSVLogger
+from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger
 from sklearn.metrics import mean_absolute_error
 from util.dataset import load_data_v1, make_submission
 import numpy as np
 import sys
 
-def mlp(sample_dim, nb_classes):
+def mlp(sample_dim,  nb_classes):
     model = Sequential()
     model.add(Dense(2048, kernel_intializer='glorot_uniform', activation='relu', input_dim=sample_dim))
     model.add(Dense(1024, kernal_intializer='glorot_uniform', activation='relu'))
@@ -20,13 +20,18 @@ if __name__ == '__main__':
     train = True if sys.argv[1] == 'train' else False
     print('****** Start Finding the Passenger ******')
     print('Loading data ...')
-    x_train, y_train, x_dev, y_dev, x_test = load_data_v1(datapath='data')
+    # 这里暂时用测试集x_test, y_test代替开发集x_dev, y_dev
+    x_train, y_train, x_dev, y_dev, x_test = load_data_v1(data_path='old_data')
+    nb_classes = np.max(y_train) + 1  # 训练集最后一层神经元的数目代表不同地理位置的类别
+    nb_test_classes = np.max(y_test) + 1  # 测试集最后一层神经元的数目代表不同地理位置的类别
+    print('nb_classes: ', nb_classes)
+    print('nb_test_classes: ', nb_test_classes)
     print('Training MLP model ...')
     check_pointer = ModelCheckpoint(filepath='models/mlp.hdf5', verbose=1, save_best_only=True,
                                     save_weights_only=True)
     early_stopping = EarlyStopping(patience=10)
     csv_logger = CSVLogger('logs/mlp')
-    mlp_model = mlp(sample_dim=x_train.shape[1])
+    mlp_model = mlp(sample_dim = x_train.shape[1], nb_classes = nb_classes)
     mlp_model.fit(x_train, y_train, batch_size=128, epochs=100, verbose=1, validation_data=(x_dev, y_dev),
                   callbacks=[check_pointer, early_stopping, csv_logger])
 
--- util/dataset.py
+++ util/dataset.py
@@ -2,37 +2,43 @@
 from __future__ import print_function
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from pandas.core.frame import DataFrame
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.feature_extraction import DictVectorizer
+from sklearn import preprocessing
+from keras.utils import np_utils
 import os
 import numpy as np
 import pandas as pd
 import datetime
 import time
 
-
 def load_data_v1(data_path):
     attr_name = ['taxi_id', 'point', 'duration', 'time', 'duration', 'distance']
-    train = pd.read_csv('train.txt', header=None)
+    # 训练集数据
+    train = pd.read_csv(os.path.join(data_path,'train.txt'), header=None)
     train_set = train.values[:,[0, 1, 2, 3, 4, 5, 6]]
+    dataset = train.values[:, [0, 1, 2, 3, 4, 5]]
     print(train_set[0])
 
-    test = pd.read_csv('test.txt', header=None)
+    # 测试集数据
+    test = pd.read_csv(os.path.join(data_path,'test.txt'), header=None)
     test_set = test.values[:, [0, 1, 2, 3, 4, 5, 6]]
     print(test_set[0])
-    dataset = train.values[:, [0, 1, 2, 3, 4, 5]]
 
+    # 测试集中除去最后一列数据存放于列表中，以出租车ID为主键
     samples = list()
     for sample in dataset:
         sample_dict = dict()
         for index, attr in enumerate(sample):
             sample_dict[attr_name[index]] = attr
         samples.append(sample_dict)
+
     h = FeatureHasher(n_features=2048)
     h.fit(samples)
-    X_train = list()
-    y_train = list()
 
-    X_test = list()
-    y_test = list()
+    # 训练集数据转换成x,y列表
+    x_train = list()
+    y_train = list()
     for sample in train_set:
         sample_dict = dict()
         for index, attr in enumerate(sample):
@@ -41,8 +47,11 @@ def load_data_v1(data_path):
                 y_train.append(int(attr))
                 continue
             sample_dict[attr_name[index]] = attr
-        X_train.append(sample_dict)
+        x_train.append(sample_dict)
 
+    # 测试集数据转换成x,y列表
+    x_test = list()
+    y_test = list()
     for sample in test_set:
         sample_dict = dict()
         for index, attr in enumerate(sample):
@@ -51,26 +60,55 @@ def load_data_v1(data_path):
                 y_test.append(int(attr))
                 continue
             sample_dict[attr_name[index]] = attr
-        X_test.append(sample_dict)
+        x_test.append(sample_dict)
 
-    X_train = h.transform(X_train).toarray()
-    X_test = h.transform(X_test).toarray()
-    print(X_train[0])
-    print(X_test[0])
-    print(X_train.shape)
-    print(X_test.shape)
+    x_train = h.transform(x_train).toarray()
+    x_test = h.transform(x_test).toarray()
+    print(x_train[0])
+    print(x_test[0])
+    print(x_train.shape)
+    print(x_test.shape)
 
     y_train = np.asarray(y_train, dtype='int16')
     y_test = np.asarray(y_test, dtype='int16')
-    nb_classes = np.max(y_train) + 1
-    nb_test_classes = np.max(y_test) + 1
-    print('nb_classes: ', nb_classes)
-    print('nb_test_classes: ', nb_test_classes)
 
     y_train = np_utils.to_categorical(y_train)
     y_test = np_utils.to_categorical(y_test, nb_classes)
     print(y_train.shape)
     print(y_test.shape)
+
+    # return x_train, y_train, x_dev, y_dev, x_test
+    return x_train, y_train, x_test, y_test, x_test
+
+# csv加载数据的方式
+def load_data_v2(data_path):
+    train_dataframe = pd.read_csv(os.path.join(data_path, 'train.csv'), header=None)
+    train_dataset = train_dataframe.values
+
+    x_train = train_dataset[:, 0:-2]
+    y_train = train_dataset[:, -2].reshape(-1, 1).astype('float32')
+
+    print('X train shape:', x_train.shape)
+    print('y train shape:', y_train.shape)
+
+    dev_dataframe = pd.read_csv(os.path.join(data_path, 'dev.csv'), header=None)
+    dev_dataset = dev_dataframe.values
+
+    x_dev = dev_dataset[:, 0:-2]
+    y_dev = dev_dataset[:, -2].reshape(-1, 1).astype('float32')
+
+    print('X dev shape:', x_dev.shape)
+    print('y dev shape:', y_dev.shape)
+
+    test_dataframe = pd.read_csv(os.path.join(data_path, 'test_public.csv'), header=None)
+    test_dataset = test_dataframe.values.astype('float32')
+
+    x_test = test_dataset
+
+    print('X test shape:', x_test.shape)
+
+    return x_train, y_train, x_dev, y_dev, x_test
+
 
 def make_submission(result_path, results, model_name):
     submit_lines = ['test_id,count\n']
@@ -83,4 +121,4 @@ def make_submission(result_path, results, model_name):
 
 
 if __name__ == '__main__':
-    load_data_v1(data_path='../data/')
+    load_data_v1(data_path='../old_data/')
